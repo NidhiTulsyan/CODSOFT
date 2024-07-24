b21bc3426ba8b501acf06383bc18889eeff726df
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2024-07-24 11:15:07 +0530

    completed hero section

--- Task_1_portfolio/portfolio/src/components/Hero/Hero.jsx
+++ Task_1_portfolio/portfolio/src/components/Hero/Hero.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hello, I'm Nidhi</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hello, I'm Nidhi</h1>
+        <p className={styles.description}>
           I am a full-stack developer with 2 Years of experince using React and
           Nodejs.Reach out if you'd like to learn more!
         </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}> Contact Me</a>
       </div>
+      <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg}/>
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
     </section>
   );
 };
