bb141670328bd95514c5c76c0e5bf9be655fc67e
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2024-07-24 10:43:38 +0530

    intitiated with Hero Section

---
+++ Task_1_portfolio/portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hello, I'm Nidhi</h1>
+        <p>
+          I am a full-stack developer with 2 Years of experince using React and
+          Nodejs.Reach out if you'd like to learn more!
+        </p>
+      </div>
+    </section>
+  );
+};
