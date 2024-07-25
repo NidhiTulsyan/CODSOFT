77b3b0aa07f0f2bbcad948eb8e810eaacec4beea
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2024-07-25 12:52:01 +0530

    added about section

--- Task_1_portfolio/portfolio/src/components/About/About.jsx
+++ Task_1_portfolio/portfolio/src/components/About/About.jsx
@@ -1,4 +1,42 @@
 import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            {/* <img src={} alt=""/> */}
+            <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm frontend developer with experience in building and optimized
+                sites
+              </p>
+            </div>
+          </li>
+
+          <li className={styles.aboutItem}>
+            {/* <img src={} alt=""/> */}
+            <img src={getImageUrl("about/serverIcon.png")} alt="server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I'm Backend developer with experience in developing fast and optimized backend system API's.
+                sites.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
